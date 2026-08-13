17a62b8e9beb0de4bbfa20b450cab6a0d3cb7990
Guillermo Palma <gpalma.olea@gmail.com>
2026-08-13 01:03:31 -0400

    favicon: gif animado como favicon, titulo Luz artificial (8) y funcion SPA sin export

--- scripts/cloudfront-function.js
+++ scripts/cloudfront-function.js
@@ -1,9 +1,9 @@
 // CloudFront Function (viewer-request) para SPA routing.
-// Pegar este codigo en la consola de AWS CloudFront > Functions > Create.
-// Asocia la funcion al comportamiento por defecto de la distribucion.
+// Publicada en la consola AWS como "luzfija-spa-router" (Runtime cloudfront-js-2.0)
+// y asociada al comportamiento por defecto de la distribucion (viewer-request).
 // Reescribe las rutas sin extension de archivo a /index.html asi el router
 // de React (BrowserRouter) funciona en rutas como /gear, /merch, etc.
-export function handler(event) {
+function handler(event) {
   var request = event.request;
   var uri = request.uri;
   var parts = uri.split("/");
